e09d45fb6d65fd1f2933faa1da16f34fa17524cd
Connor Duggan <connorduggan@Connors-iMac27.local>
2012-10-25 01:53:06 -0700

    Init

---
+++ PFObject+More.h
@@ -0,0 +1,15 @@
+//
+//  PFObject+More.h
+//  CDParse
+//
+//  Created by Connor Duggan on 12-08-05.
+//  Copyright (c) Connor Duggan. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+@interface PFObject (More)
+
+-(BOOL)saving;
+
+@end
