823649ceaeb6fe3f00e17548161ea5c7a9a03010
Connor Duggan <connorduggan@Connors-iMac27.local>
2012-10-26 17:04:04 -0700

    added .gitignore. created XCode project

---
+++ CDParse/CDParse/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CDParse
+//
+//  Created by Connor Duggan on 2012-10-26.
+//  Copyright (c) 2012 Connor Duggan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CDParse/CDParse/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CDParse
+//
+//  Created by Connor Duggan on 2012-10-26.
+//  Copyright (c) 2012 Connor Duggan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
